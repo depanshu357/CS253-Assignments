89ca143bbc46fdfbdd37129472c3fadd4d7ee529
Depanshu <depanshusahu057@gmail.com>
2023-04-02 10:25:21 +0530

    comments removed

--- Shell_Scripting/players.sh
+++ Shell_Scripting/players.sh
@@ -9,7 +9,5 @@ tmp_file2=$(mktemp)
 sed 's/Goalkeeper/111/g; s/Defender/222/g; s/Midfielder/333/g; s/Forward/444/g' "$1" > "$tmp_file"
 sort -t ',' -k16,16nr -k7,7 "$tmp_file" > "$tmp_file1"
 sed  's/111/Goalkeeper/g; s/222/Defender/g; s/333/Midfielder/g; s/444/Forward/g' "$tmp_file1" > "$tmp_file2"
-# sed -i '$ d' "$tmp_file2" > "$tmp_file1"
-# head -n -2 "$tmp_file2" > "$tmp_file1"
 echo "full_name,position,nationality,goals_overall" > "$2"
 awk -F',' '{print $1 "," $7 "," $12 "," $16}' "$tmp_file2" >> "$2"
