c1ff929408e11bb13ad05f4af891e42e548ac56e
Depanshu <depanshusahu057@gmail.com>
2023-04-02 10:38:04 +0530

    first ques of ass. 1 corrected solution

--- Shell_Scripting/comm.sh
+++ Shell_Scripting/comm.sh
@@ -11,17 +11,20 @@ fi
 tmp_file1=$(mktemp)
 tmp_file2=$(mktemp)
 
-sed -i '/^$/d' "$1"  
-sed -i '/^$/d' "$2" 
+grep -v '^$' "$1" > "$tmp_file1"
+grep -v '^$' "$2" > "$tmp_file2"
+
+sed -i '/^$/d' "$tmp_file1"  
+sed -i '/^$/d' "$tmp_file2" 
 
 while read line1;
 do
 	while read line2; 
 	do
-		if [[ "$line1" =~ ^${line2}\s*$ ]] || [[ "$line2" =~ ^${line1}\s*$ ]]
+		if [[ ^${line1}\s*$ =~ ^${line2}\s*$ ]] || [[ "$line2" =~ ^${line1}\s*$ ]]
 		then
 			echo "$line1"
 			break
 		fi
-	done < $2
-done < $1
+	done < $tmp_file2
+done < $tmp_file1
